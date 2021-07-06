b9cb10f53801a37b839be806a767cf01a81cf871
pranav21999 <pranavdani99@gmail.com>
2021-07-07 01:11:35 +0530

    Updated: method for Validation of password

--- UserRegistration.java
+++ UserRegistration.java
@@ -78,7 +78,6 @@ public class UserRegistration {
 	     } 
 	}
 
-
 	//method to check password Valid or Invalid
 	 public void validatePassword() {
 		 System.out.println("Enter Password");
@@ -86,9 +85,10 @@ public class UserRegistration {
 		 /*regex pattern for password:
 		  * 1)must contain atleast 8 characters 
 		  * 2)must contain one UpperCase
-		  * 3)should have 1 numericno.
+		  * 3)should have 1 numericno
+		  * 4)has contain Exactily one specialSymbol.
 		 */
-		 String regex = "^[A-Z]{1}+[a-zA-z1-9]{6,}[1-9]{1}$";
+		 String regex = "^[A-Z]{1}+[a-zA-z1-9]{5,}[@$^]{1}[1-9]{1}$";
 		 Pattern p = Pattern.compile(regex);
 		 Matcher matcher = p.matcher(password);
 		 boolean result = matcher.matches();
@@ -100,6 +100,7 @@ public class UserRegistration {
 	    	 System.out.println("Invalid Password");
 	     } 
 	}
+	
 
 	//Main method
 	public static void main(String[] args) {
@@ -123,11 +124,11 @@ public class UserRegistration {
 				break;
 			case 5:
 				registration.validatePassword();
-				break;		
+				break;
 			default:
 				System.out.println("Invalid choice");
 				System.exit(0);
 			}
-		}	
+		}
 	}
 }
